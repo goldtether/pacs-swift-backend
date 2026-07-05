0df9c4eafbb8d78e005cae8c3eb02d889470f935
goldtether <gold.tether.io@gmail.com>
2026-07-05 17:52:52 +0500

    Create database.js

---
+++ src/config/database.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+module.exports = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    logging: false,
+    pool: {
+      max: 10,
+      min: 2,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
+);
